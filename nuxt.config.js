235d9979fa8cf654e1270990f75a4bd0422e6092
Bryce Miller <bmiller@mid-city.com>
2021-08-27 09:39:25 -0400

    update packages, tweak config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,41 +3,64 @@
 // Set sanity options in one place, for use in nuxt config and dynamicRoutes
 const sanityClient = require('@sanity/client')
 const sanityOpts = {
-  projectId: 'hxed9pbn',
-  dataset: 'prod',
-  token:
-    'skXdaQLTMgIqJElbOVXo3Ox8BuIsseBlgL40cnuEfWIckicqHT41Zh1C8jvwvgIWKN5P0lAo0SOlKmoyTZOoPDsEhmYw891H0AJ75dUiU7rHefQO0RfSc5iJq3bhFekOODUynCyU2a9jqHgSJs6npBYCLVcw4NcSZoUKKFJpCQfdlyZwX691',
+  projectId: process.env.SANITY_PROJECT,
+  dataset: process.env.SANITY_DATASET,
   useCdn: false,
 }
-const sanity = sanityClient(sanityOpts)
+const sanity = sanityClient({
+  token: process.env.SANITY_TOKEN,
+  ...sanityOpts,
+})
 
 // generates array of dynamic routes for generate.routes
 let dynamicRoutes = () => {
-  return sanity.fetch('*[]._id').then((res) => {
-    return res.flatMap((id) => [`/${id}`, `/${id}/details`, `/${id}/go`])
+  return sanity.fetch('*[]._id').then(res => {
+    return res.flatMap(id => [`/${id}`, `/${id}/details`, `/${id}/go`])
   })
 }
 
 // rest of config
 export default {
   target: 'static',
+
   components: true,
+
   modules: ['@nuxtjs/axios'],
-  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/fontawesome', '@nuxtjs/sanity'],
+
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/fontawesome',
+    '@nuxtjs/sanity/module',
+  ],
+
   plugins: ['~/plugins/vue-gtag.js'],
+
   css: ['~/assets/css/main.css'],
+
   head: {
-    title: 'Mid-City MPP 2021',
+    title: 'Mid-City MPP 2022',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
   },
+
   generate: {
     routes: dynamicRoutes,
   },
 
-  sanity: { minimal: true, ...sanityOpts },
+  server: {
+    host: '10.34.1.49',
+  },
+
+  sanity: sanityOpts,
+
+  privateRuntimeConfig: {
+    sanity: {
+      token: process.env.SANITY_TOKEN,
+    },
+  },
+
   fontawesome: {
     component: 'Fa',
     suffix: true,
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,28 +7,28 @@ module.exports = {
   // purge: {
   //   mode: 'all',
   // },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/ui')],
+  plugins: [require("@tailwindcss/typography")],
   variants: {
-    backgroundColor: ['hover', 'focus'],
+    backgroundColor: ["hover", "focus"],
   },
   theme: {
     extend: {
       colors: {
         brand: {
-          normal: '#C1262E',
-          light: '#CF2932',
-          lightest: '#E6BCBE',
-          dark: '#A82228',
-          darkest: '#821A1F',
+          normal: "#C1262E",
+          light: "#CF2932",
+          lightest: "#E6BCBE",
+          dark: "#A82228",
+          darkest: "#821A1F",
         },
       },
       fontSize: {
-        '7xl': '6rem',
+        "7xl": "6rem",
       },
       inset: {
-        '-1': '-0.25rem',
-        '-44': '-11rem',
+        "-1": "-0.25rem",
+        "-44": "-11rem",
       },
     },
   },
-}
+};
