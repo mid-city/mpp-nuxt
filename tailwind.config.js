e0c6286e56aae3e74ecc0265e3812b488be2d1f9
Bryce Miller <bmiller@mid-city.com>
2021-10-12 16:00:55 -0400

    hero wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,28 +7,35 @@ module.exports = {
   // purge: {
   //   mode: 'all',
   // },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
   variants: {
-    backgroundColor: ["hover", "focus"],
+    backgroundColor: ['hover', 'focus'],
   },
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
+      backgroundImage: {
+        'hero-image':
+          "url('https://res.cloudinary.com/mid-city/image/upload/f_auto,q_auto/v1634066095/MPP/AdobeStock_192180956_t9gj61.jpg')",
+      },
       colors: {
         brand: {
-          normal: "#C1262E",
-          light: "#CF2932",
-          lightest: "#E6BCBE",
-          dark: "#A82228",
-          darkest: "#821A1F",
+          normal: '#C1262E',
+          light: '#CF2932',
+          lightest: '#E6BCBE',
+          dark: '#A82228',
+          darkest: '#821A1F',
         },
       },
       fontSize: {
-        "7xl": "6rem",
+        '7xl': '6rem',
       },
       inset: {
-        "-1": "-0.25rem",
-        "-44": "-11rem",
+        '-1': '-0.25rem',
+        '-44': '-11rem',
       },
     },
   },
-};
+}
