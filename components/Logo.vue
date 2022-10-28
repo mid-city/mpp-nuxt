a382384834b2eda752810771f89e560de17a87fa
Bryce Miller <sandprickle@users.noreply.github.com>
2022-10-28 11:27:18 -0400

    update logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -2,10 +2,10 @@
   <div class="w-28 sm:w-32 lg:w-40">
     <a href="https://www.mid-city.com/" target="_blank" rel="noopener">
       <NuxtImg
-        src="Logos/MID-CITY_SUPPLY_75TH_red-black_voslqb"
+        src="products/img/MID-CITY_SUPPLY_Vector_Red_Blk_email"
         alt="Mid-City Logo"
         width="698"
-        height="641"
+        height="353"
         sizes="xs:112px sm:128px lg:160px"
       />
     </a>
