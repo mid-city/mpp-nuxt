d0c3b57796ed380b0703d4fac3fed962b16b903b
Bryce Miller <sandprickle@users.noreply.github.com>
2022-11-06 20:24:13 -0500

    greeting styles

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,108 +2,31 @@
   <div class="relative bg-stuff">
     <div class="container">
       <div
-        class="
-          relative
-          z-10
-          pb-8
-          sm:pb-16
-          md:pb-20
-          lg:max-w-2xl
-          lg:w-full
-          lg:pb-28
-          xl:pb-32
-        "
+        class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
       >
         <HeroHeader />
         <main
-          class="
-            pt-10
-            mx-auto
-            max-w-screen-xl
-            px-4
-            sm:px-6
-            md:pt-16
-            lg:pt-20
-            lg:px-8
-            xl:pt-28
-            relative
-          "
+          class="pt-10 mx-auto max-w-screen-xl px-4 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28 relative"
         >
           <div class="sm:text-center lg:text-left">
             <Greeting
               v-if="showGreeting"
               :name="greetingName"
-              class="
-                text-brand-dark
-                absolute
-                top-2
-                lg:top-4
-                xl:top-6
-                w-full
-                lg:w-auto
-              "
+              class="text-gray-200 absolute top-2 lg:top-4 xl:top-6 w-full lg:w-auto"
             />
             <h2
-              class="
-                lg:relative
-                lg:-top-8
-                xl:-top-10
-                hero-text
-                text-4xl
-                leading-10
-                tracking-tight
-                sm:tracking-normal
-                font-extrabold
-                text-gray-200
-                sm:text-5xl
-                sm:leading-none
-                md:text-6xl
-                xl:text-7xl
-                h-20
-                sm:h-auto
-              "
+              class="lg:relative lg:-top-8 xl:-top-10 hero-text text-4xl leading-10 tracking-tight sm:tracking-normal font-extrabold text-gray-200 sm:text-5xl sm:leading-none md:text-6xl xl:text-7xl h-20 sm:h-auto"
             >
               2023 Marketing Partnership Plan
             </h2>
             <Slogan class="lg:hidden" />
             <div
-              class="
-                relative
-                mt-5
-                sm:mt-8
-                sm:flex
-                sm:justify-center
-                lg:justify-start
-              "
+              class="relative mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
             >
               <div class="rounded-md shadow">
                 <NuxtLink
                   :to="pathPrefix + 'go'"
-                  class="
-                    w-full
-                    flex
-                    items-center
-                    justify-center
-                    px-8
-                    py-3
-                    border border-transparent
-                    text-base
-                    leading-6
-                    font-medium
-                    rounded-md
-                    text-gray-100
-                    bg-brand-normal
-                    hover:bg-brand-light
-                    focus:outline-none
-                    focus:border-brand-dark
-                    focus:shadow-outline-indigo
-                    transition
-                    duration-150
-                    ease-in-out
-                    md:py-4
-                    md:text-lg
-                    md:px-10
-                  "
+                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100 bg-brand-normal hover:bg-brand-light focus:outline-none focus:border-brand-dark focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                 >
                   Get started
                 </NuxtLink>
@@ -111,32 +34,7 @@
               <div class="mt-3 sm:mt-0 sm:ml-3">
                 <NuxtLink
                   :to="pathPrefix + 'details'"
-                  class="
-                    w-full
-                    flex
-                    items-center
-                    justify-center
-                    px-8
-                    py-3
-                    border border-gray-300
-                    text-base
-                    leading-6
-                    font-medium
-                    rounded-md
-                    text-gray-700
-                    bg-gray-300
-                    hover:text-brand-normal
-                    hover:bg-gray-100
-                    focus:outline-none
-                    focus:shadow-outline-indigo
-                    focus:border-indigo-300
-                    transition
-                    duration-150
-                    ease-in-out
-                    md:py-4
-                    md:text-lg
-                    md:px-10
-                  "
+                  class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 text-base leading-6 font-medium rounded-md text-gray-700 bg-gray-300 hover:text-brand-normal hover:bg-gray-100 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                 >
                   Details
                 </NuxtLink>
